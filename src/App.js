4bc96f174f2307f4316b15d41352a5d02d458a47
Carter Fuchs <carterjfuchs@gmail.com>
2024-09-11 15:02:34 -0600

    Initial commit

--- src/App.js
+++ src/App.js
@@ -5,18 +5,110 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div className="Guesses-grid">
+          <div className="Guess-row">
+            <div className="Tile">
+              e
+            </div>
+            <div className="Tile">
+              e
+            </div>
+            <div className="Tile">
+              e
+            </div>
+            <div className="Tile">
+              e
+            </div>
+            <div className="Tile">
+              e
+            </div>
+          </div>
+          <div className="Guess-row">
+            <div className="Tile">
+              e
+            </div>
+            <div className="Tile">
+              e
+            </div>
+            <div className="Tile">
+              e
+            </div>
+            <div className="Tile">
+              e
+            </div>
+            <div className="Tile">
+              e
+            </div>
+          </div>
+          <div className="Guess-row">
+            <div className="Tile">
+              e
+            </div>
+            <div className="Tile">
+              e
+            </div>
+            <div className="Tile">
+              e
+            </div>
+            <div className="Tile">
+              e
+            </div>
+            <div className="Tile">
+              e
+            </div>
+          </div>
+          <div className="Guess-row">
+            <div className="Tile">
+              e
+            </div>
+            <div className="Tile">
+              e
+            </div>
+            <div className="Tile">
+              e
+            </div>
+            <div className="Tile">
+              e
+            </div>
+            <div className="Tile">
+              e
+            </div>
+          </div>
+          <div className="Guess-row">
+            <div className="Tile">
+              e
+            </div>
+            <div className="Tile">
+              e
+            </div>
+            <div className="Tile">
+              e
+            </div>
+            <div className="Tile">
+              e
+            </div>
+            <div className="Tile">
+              e
+            </div>
+          </div>
+          <div className="Guess-row">
+            <div className="Tile">
+              e
+            </div>
+            <div className="Tile">
+              e
+            </div>
+            <div className="Tile">
+              e
+            </div>
+            <div className="Tile">
+              e
+            </div>
+            <div className="Tile">
+              e
+            </div>
+          </div>
+        </div>
       </header>
     </div>
   );
