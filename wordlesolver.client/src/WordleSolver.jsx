b162b4ef27f512e9d87002b92d3120d89688eb3c
Carter Fuchs <carterjfuchs@gmail.com>
2024-11-05 15:06:21 -0700

    Adds ability to create, remove and clear guesses

--- wordlesolver.client/src/WordleSolver.jsx
+++ wordlesolver.client/src/WordleSolver.jsx
@@ -3,6 +3,13 @@ import WordleGuess from "./components/WordleGuess";
 import "./WordleSolver.css";
 
 function WordleSolver() {
+  /*
+  Color legend:
+  1 - clear
+  2 - yellow
+  3 - green
+  */
+
   const [guesses, setGuesses] = useState([
     { letters: ["", "", "", "", ""], colors: [1, 1, 1, 1, 1] },
   ]);
@@ -10,6 +17,29 @@ function WordleSolver() {
   const handleChangeGuess = (newGuess, i) => {
     let newGuesses = [...guesses];
     newGuesses[i] = newGuess;
+    setGuesses(newGuesses);
+  };
+
+  const handleAddGuess = (guessIndex) => {
+    // Only add a new guess if the guess where the user hit enter is the last guess
+    if (guessIndex === guesses.length - 1) {
+      console.log("Adding new guess");
+      let newGuesses = [...guesses];
+      newGuesses.push({
+        letters: ["", "", "", "", ""],
+        colors: [1, 1, 1, 1, 1],
+      });
+      setGuesses(newGuesses);
+    }
+  };
+
+  const checkIfDeletable = () => {
+    return guesses.length > 1;
+  }
+
+  const handleDeleteGuess = (index) => {
+    let newGuesses = [...guesses];
+    newGuesses.splice(index, 1);
     setGuesses(newGuesses);
   }
 
@@ -21,6 +51,9 @@ function WordleSolver() {
             <WordleGuess
               guess={guess}
               setGuess={(newGuess) => handleChangeGuess(newGuess, i)}
+              addGuess={() => handleAddGuess(i)}
+              isDeletable={checkIfDeletable}
+              deleteGuess={() => handleDeleteGuess(i)}
               key={i}
             />
           );
--- wordlesolver.client/src/components/WordleGuess.jsx
+++ wordlesolver.client/src/components/WordleGuess.jsx
@@ -1,73 +1,131 @@
 import WordleLetter from "./WordleLetter";
 import "./WordleGuess.css";
 import { useState, useRef, useEffect } from "react";
+import binLogo from "../assets/bin.png";
+import refreshLogo from "../assets/refresh.png";
 
 function WordleGuess(props) {
+  const [showSideButtons, setShowSideButtons] = useState(false);
   const letterRefs = useRef([]);
 
   useEffect(() => {
-    letterRefs.current = letterRefs.current.slice(0, props.guess.letters.length)
-  }, [props.guess.letters])
+    letterRefs.current = letterRefs.current.slice(
+      0,
+      props.guess.letters.length
+    );
+    letterRefs.current[0].focus();
+  }, []);
 
   const handleSetLetter = (newLetter, i) => {
-    let newLetters = [...props.guess.letters]
+    let newLetters = [...props.guess.letters];
     newLetters[i] = newLetter;
     props.setGuess({
       letters: newLetters,
-      colors: props.guess.colors
-    })
-  }
+      colors: props.guess.colors,
+    });
+  };
 
   const handleSetColor = (newColor, i) => {
     let newColors = [...props.guess.colors];
     newColors[i] = newColor;
     props.setGuess({
       letters: props.guess.letters,
-      colors: newColors
+      colors: newColors,
     });
-  }
+  };
 
   const handleGoBack = (index) => {
     if (index > 0) {
       letterRefs.current[index - 1].focus();
     }
-  }
-  
+  };
+
   const handleGoForward = (index) => {
     if (index < props.guess.letters.length - 1) {
-      letterRefs.current[index + 1].focus()
+      letterRefs.current[index + 1].focus();
     }
-  }
+  };
 
   const handleBackspace = (index) => {
     if (index > 0) {
       handleSetLetter("", index - 1);
       handleGoBack(index);
     }
-  }
+  };
 
   const handleSubmit = (index) => {
+    if (
+      props.guess.letters.every((letter) => {
+        return letter !== "";
+      })
+    ) {
+      props.addGuess();
+    }
     letterRefs.current[index].blur();
-  }
-  
+  };
+
+  const handleMouseEnter = () => {
+    setShowSideButtons(true);
+  };
+
+  const handleMouseLeave = () => {
+    setShowSideButtons(false);
+  };
+
+  const handleDeleteRow = () => {
+    props.deleteGuess();
+  };
+
+  const handleClearRow = () => {
+    props.setGuess({ letters: ["", "", "", "", ""], colors: [1, 1, 1, 1, 1] });
+  };
+
   return (
-    <div className="guess">
-      {props.guess.letters.map((letter, i) => {
-        return (
-          <WordleLetter
-            key={i}
-            ref={x => letterRefs.current[i] = x}
-            letter={letter}
-            color={props.guess.colors[i]}
-            setLetter={(newLetter) => handleSetLetter(newLetter, i)}
-            setColor={(newColor) => handleSetColor(newColor, i)}
-            goBack={() => handleGoBack(i)}
-            goForward={() => handleGoForward(i)}
-            backspace={() => handleBackspace(i)}
-            submit={() => handleSubmit(i)}
-          />
-        );
-      })}
+    <div
+      className="guessRow"
+      onMouseEnter={handleMouseEnter}
+      onMouseLeave={handleMouseLeave}
+    >
+      <div className="guess">
+        <div className="guessLetters">
+          {props.guess.letters.map((letter, i) => {
+            return (
+              <WordleLetter
+                key={i}
+                ref={(x) => (letterRefs.current[i] = x)}
+                letter={letter}
+                color={props.guess.colors[i]}
+                setLetter={(newLetter) => handleSetLetter(newLetter, i)}
+                setColor={(newColor) => handleSetColor(newColor, i)}
+                goBack={() => handleGoBack(i)}
+                goForward={() => handleGoForward(i)}
+                backspace={() => handleBackspace(i)}
+                submit={() => handleSubmit(i)}
+              />
+            );
+          })}
+          {showSideButtons && (
+            <div className="sideButtons">
+              <button
+                className="sideButton"
+                title="Clear row"
+                onClick={handleClearRow}
+              >
+                <img className="buttonLogo" src={refreshLogo} />
+              </button>
+              {props.isDeletable() && (
+                <button
+                  className="sideButton"
+                  title="Delete row"
+                  onClick={handleDeleteRow}
+                >
+                  <img className="buttonLogo" src={binLogo} />
+                </button>
+              )}
+            </div>
+          )}
+        </div>
+      </div>
     </div>
   );
 }
