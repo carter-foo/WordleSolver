42b350f987b9b5c5f7c1a0e982c7ed956c131069
Carter Fuchs <carterjfuchs@gmail.com>
2025-03-19 22:34:08 -0600

    add functionality to cover most guess cases

--- WordleSolver.Server/Solver/WordleRegexBuilder.cs
+++ WordleSolver.Server/Solver/WordleRegexBuilder.cs
@@ -1,22 +1,88 @@
 ﻿namespace WordleSolver.Server.Solver {
     public class WordleRegexBuilder {
+        private Dictionary<int, char> _knownLetters;
+        private Dictionary<int, char> _restrictedLetters;
+        private List<char> excludedLetters;
+
+        public WordleRegexBuilder() {
+            _knownLetters = [];
+            _restrictedLetters = [];
+            excludedLetters = [];
+        }
+
         // Sets a letter slot to the given letter
         public WordleRegexBuilder SetLetter(char letter, int position) {
+            _knownLetters[position] = letter;
             return this;
         }
 
         // Removes the letter as a potential option from all slots
         public WordleRegexBuilder ExcludeLetter(char letter) {
+            excludedLetters.Add(letter);
             return this;
         }
 
         // Restricts the letter from the letter slot, but adds a requirement that the letter exists somewhere 
         public WordleRegexBuilder RestrictLetter(char letter, int position) {
+            _restrictedLetters[position] = letter;
             return this;
         }
 
         public string Build() {
-            return "";
+            /*
+             * For each position:
+             * 1. If the position is known, add the letter and move to the next
+             * 2. If there are particular restrictions for the position, add the restriction to the pattern
+             * 3. Add restrictions for each excluded letter
+             * 
+             * At the end, add a lookahead for each restricted letter
+             */
+
+            bool needsLookahead = _restrictedLetters.Count > 0;
+
+            string result = "^";
+
+            if (needsLookahead) {
+                result += "(?=";
+            }
+
+            for (int i = 0; i < 5; i++) { 
+                if(_knownLetters.ContainsKey(i)) {
+                    result += _knownLetters[i];
+                    continue;
+                }
+
+                var restrictions = new List<char>();
+                if (_restrictedLetters.ContainsKey(i)) {
+                    restrictions.Add(_restrictedLetters[i]);
+                }
+                restrictions.AddRange(excludedLetters);
+
+                if (restrictions.Count > 0) {
+                    string addition = "[^";
+                    foreach (char restriction in restrictions) {
+                        addition += $"{restriction}";
+                    }
+                    addition += "]";
+                    result += addition;
+                } else {
+                    result += ".";
+                }
+                
+            }
+
+            result += "$";
+
+            if (needsLookahead) {
+                result += ")";
+            }
+
+            foreach(var restriction in _restrictedLetters) {
+                result += $"(?=.*{restriction.Value})";
+            }
+            result += ".*";
+
+            return result;
         }
     }
 }
--- WordleSolver.Tests/Solver/WordleRegexBuilderTests.cs
+++ WordleSolver.Tests/Solver/WordleRegexBuilderTests.cs
@@ -9,7 +9,7 @@ using WordleSolver.Server.Solver;
 namespace WordleSolver.Tests.Solver {
     public class WordleRegexBuilderTests {
         [Fact]
-        public void SetLetter_ShouldGivePatternThatMatchesWordWithLetter() {
+        public void SetLetter_ShouldMatchWordWithLetter() {
             // Arrange
             var builder = new WordleRegexBuilder();
 
@@ -20,6 +20,49 @@ namespace WordleSolver.Tests.Solver {
             // Assert
             Assert.Matches(pattern, "apple");
             Assert.DoesNotMatch(pattern, "lions");
+        }
+
+        [Fact]
+        public void ExcludeLetter_ShouldNotMatchWordWithLetter() {
+            // Arrange
+            var builder = new WordleRegexBuilder();
+            
+            // Act
+            builder.ExcludeLetter('a');
+            string pattern = builder.Build();
+
+            // Assert
+            Assert.DoesNotMatch(pattern, "apple");
+            Assert.Matches(pattern, "lions");
+        }
+
+        [Fact]
+        public void RestrictLetter_ShouldMatchWordWithLetterInAnyOtherPosition() {
+            // Arrange
+            var builder = new WordleRegexBuilder();
+            
+            // Act
+            builder.RestrictLetter('a', 0);
+            string pattern = builder.Build();
+            
+            // Assert
+            Assert.DoesNotMatch(pattern, "apple");
+            Assert.DoesNotMatch(pattern, "lions");
+            Assert.Matches(pattern, "place");
+        }
+
+        [Fact]
+        public void SetLetterAndRestrictSameLetter_ShouldNotMatchWordWithOnlySetLetter() {
+            // Arrange
+            var builder = new WordleRegexBuilder();
+
+            // Act
+            builder.SetLetter('p', 1);
+            builder.RestrictLetter('p', 0);
+            string pattern = builder.Build();
+
+            // Assert
+            Assert.DoesNotMatch(pattern, "spend");
         }
     }
 }
